69f4f559b2c4b001d92f124b0ccd2b397b1d0867
ishanrana363 <ishanrana363@gmail.com>
2025-05-03 12:50:31 -0800

    otp verify page design completed

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,7 @@ import MainLayout from "../layout/MainLayout";
 import RegisterForm from "../components/client/register-from/RegisterForm";
 import LoginFrom from "../components/client/login-from/LoginFrom";
 import EmailVerify from "../components/client/forget-password/EmailVerify";
+import OtpVerify from "../components/client/forget-password/OtpVerify";
 
 export const router = createBrowserRouter([
     {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
             {
                 path : "email-verify",
                 element : <EmailVerify></EmailVerify>
+            },
+            {
+                path : "otp-verify",
+                element : <OtpVerify></OtpVerify>
             }
         ]
     }
