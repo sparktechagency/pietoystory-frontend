58dad5ea1fb30dc66956db1b7817e952f433bbd1
ishanrana363 <ishanrana363@gmail.com>
2025-05-05 11:39:49 -0800

    about page design completed

--- src/pages/client/home-page/Home.tsx
+++ src/pages/client/home-page/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import PromisseRefer from './PromisseRefer'
+import About from './About'
 
-const Home : React.FC = () => {
+const Home: React.FC = () => {
     return (
         <div>
             <Banner></Banner>
             <PromisseRefer></PromisseRefer>
+            <div id='about' >
+                <About></About>
+            </div>
         </div>
     )
 }
