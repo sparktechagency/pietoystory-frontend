9c8f86d113c38c562d214c6a1aa6b8fa872c7633
ishanrana363 <ishanrana363@gmail.com>
2025-05-05 09:26:13 -0800

    vercel file added

--- src/pages/client/home-page/Home.tsx
+++ src/pages/client/home-page/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import PromisseRefer from './PromisseRefer'
 
 const Home : React.FC = () => {
     return (
         <div>
             <Banner></Banner>
+            <PromisseRefer></PromisseRefer>
         </div>
     )
 }
